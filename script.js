6e4f623bdf1cc8047cce9d8bf64b46f9094a2d88
n4ndee <36826444+n4ndee@users.noreply.github.com>
2024-07-04 02:00:21 +0200

    Implement chart

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const incomeValue = document.querySelector(".income__value");
-const returnValue = document.querySelector("#return");
+const returnValue = document.querySelector("#return-input");
 const slider = document.querySelector(".slider");
 const depositValue = document.querySelector(".deposit__value");
 
@@ -21,3 +21,77 @@ returnValue.addEventListener("input", function () {
   depositValue.innerHTML = slider.value;
   incomeValue.innerHTML = Math.trunc(slider.value * returnPercent);
 });
+
+///////////////////////////////////////
+// Chart setup
+
+const labels = [
+  "1 Year",
+  "2 Years",
+  "3 Years",
+  "4 Years",
+  "5 Years",
+  "6 Years",
+  "7 Years",
+  "8 Years",
+  "9 Years",
+  "10 Years",
+];
+
+const colors = {
+  color1: {
+    border: "cornflowerblue",
+    background: "lightskyblue",
+  },
+  color2: {
+    border: "mediumspringgreen",
+    background: "palegreen",
+  },
+};
+
+const data1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const data2 = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50];
+
+const data = {
+  labels: labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: data1,
+      borderColor: colors.color1.border,
+      backgroundColor: colors.color1.background,
+    },
+    {
+      label: "Dataset 2",
+      data: data2,
+      borderColor: colors.color2.border,
+      backgroundColor: colors.color2.background,
+    },
+  ],
+};
+
+///////////////////////////////////////
+// Chart config
+
+const config = {
+  type: "line",
+  data: data,
+  options: {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "bottom",
+      },
+      title: {
+        display: true,
+        text: "Based on last year's performance of an avg. 5% / month",
+      },
+    },
+  },
+};
+
+///////////////////////////////////////
+// Chart display
+
+const chartEl = document.getElementById("chart");
+new Chart(chartEl, config);
