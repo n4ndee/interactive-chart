dba8e03c2de98606b4a3f317e2adf2c547f402b1
n4ndee <36826444+n4ndee@users.noreply.github.com>
2024-07-16 03:30:40 +0200

    Update layout

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 "use strict";
 
-///////////////////////////////////////
-// Inputs
+const incomeValue = document.querySelector(".form__income .form-value");
+const depositValue = document.querySelector(".form__deposit .form-value");
+const inputReturn = document.querySelector(".form__return .form-input");
+const inputDeposit = document.querySelector(".form__deposit .form-input");
+const checkboxInflation = document.querySelector(
+  ".form__inflation .form-input"
+);
 
-const incomeValue = document.querySelector(".form__income--value");
-const depositValue = document.querySelector(".form__deposit--value");
-const inputReturn = document.querySelector(".form__input--return");
-const inputDeposit = document.querySelector(".form__input--deposit");
+// Calculate income
 
 let returnPercent;
 
 const calcIncome = function () {
+  if (!inputReturn.checkValidity()) return;
+
   returnPercent = inputReturn.value / 100;
 
   depositValue.textContent = inputDeposit.value;
@@ -18,29 +22,45 @@ const calcIncome = function () {
 };
 calcIncome();
 
-///////////////////////////////////////
-// Logic
+// Update dataset
 
 const years = 10;
 const MONTHS = 12;
+let inflation = 1;
 
 let data1 = [+inputDeposit.value];
 let data2 = [+incomeValue.textContent];
 
 const updateDataset = function (arr, percentage, years) {
   for (let i = 0; i < years; i++) {
-    arr.push(Math.trunc(arr[i] + arr[i] * percentage * MONTHS));
+    arr.push(Math.trunc(arr[i] + arr[i] * percentage * MONTHS * inflation));
   }
 };
-
 updateDataset(data1, returnPercent, years);
 updateDataset(data2, returnPercent, years);
+
+// Update chart
+
+const updateChart = function () {
+  if (!inputReturn.checkValidity()) return;
+
+  data1.length = 1;
+  data2.length = 1;
+
+  data1[0] = +inputDeposit.value;
+  data2[0] = +incomeValue.textContent;
+
+  updateDataset(data1, returnPercent, years);
+  updateDataset(data2, returnPercent, years);
+
+  chart.update();
+};
 
 ///////////////////////////////////////
 // Chart setup
 
 const labels = [
-  "Beginning",
+  "Start",
   "1 Year",
   "2 Years",
   "3 Years",
@@ -53,9 +73,15 @@ const labels = [
   "10 Years",
 ];
 
+const root = document.documentElement;
+
 const colors = {
-  color1: "cornflowerblue",
-  color2: "mediumspringgreen",
+  primary: getComputedStyle(root).getPropertyValue("--primary"),
+  secondary: getComputedStyle(root).getPropertyValue("--secondary"),
+  font: getComputedStyle(root).getPropertyValue("--font"),
+  gray: getComputedStyle(root).getPropertyValue("--gray"),
+  grayMedium: getComputedStyle(root).getPropertyValue("--gray-medium"),
+  grayDark: getComputedStyle(root).getPropertyValue("--gray-dark"),
 };
 
 const data = {
@@ -64,14 +90,14 @@ const data = {
     {
       label: "Balance",
       data: data1,
-      borderColor: colors.color1,
-      backgroundColor: colors.color1,
+      borderColor: colors.primary,
+      backgroundColor: colors.primary,
     },
     {
       label: "Profit",
       data: data2,
-      borderColor: colors.color2,
-      backgroundColor: colors.color2,
+      borderColor: colors.secondary,
+      backgroundColor: colors.secondary,
     },
   ],
 };
@@ -85,8 +111,7 @@ const config = {
   options: {
     responsive: true,
     maintainAspectRatio: false,
-    pointRadius: 5,
-    pointHoverRadius: 10,
+    pointHoverRadius: 5,
     interaction: {
       intersect: false,
       mode: "index",
@@ -99,15 +124,23 @@ const config = {
         display: true,
         text: `Based on previous years' avg. performance of ${
           inputReturn.value
-        }% / month (${inputReturn.value * 12}% / yr)`,
+        }% / month (${Math.trunc(inputReturn.value * 12)}% / yr)`,
       },
       tooltip: {
-        backgroundColor: "gray",
-        padding: 12,
-        titleFont: { size: "16" },
         position: "average",
         yAlign: "bottom",
         caretSize: 0,
+        backgroundColor: colors.gray,
+        titleColor: colors.font,
+        bodyColor: colors.font,
+        borderColor: colors.grayDark,
+        borderWidth: 1,
+        cornerRadius: 0,
+        padding: 12,
+        titleFont: { size: 18 },
+        bodyFont: { size: 14 },
+        boxPadding: 6,
+        multiKeyBackground: "none",
       },
     },
   },
@@ -119,22 +152,14 @@ const config = {
 const chartEl = document.getElementById("chart");
 const chart = new Chart(chartEl, config);
 
-const updateChart = function () {
-  data1.length = 1;
-  data2.length = 1;
-
-  data1[0] = +inputDeposit.value;
-  data2[0] = +incomeValue.textContent;
-
-  updateDataset(data1, returnPercent, years);
-  updateDataset(data2, returnPercent, years);
-
-  chart.update();
-};
-
 inputDeposit.addEventListener("input", function () {
   calcIncome();
   inputReturn.blur();
+});
+
+checkboxInflation.addEventListener("change", function () {
+  inflation = this.checked ? 0.97 : 1;
+  updateChart();
 });
 
 inputDeposit.addEventListener("change", function () {
